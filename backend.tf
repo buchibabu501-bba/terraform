b9ad88f8ec925bef0660018c748f884198ca65fa
Buchi Babu Addagada <313687988+buchibabu501-bba@users.noreply.github.com>
2026-08-10 22:56:52 -0500

    Remove trailing space from S3 bucket name in backend configuration

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "amzn-terraform-bucket-babu "
+    bucket         = "amzn-terraform-bucket-babu"
     key            = "terraform/state.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terrafrom-babu"
